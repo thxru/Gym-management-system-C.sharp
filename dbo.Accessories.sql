ecd4a6c4e373a94c80518f1c35b70d1f651d9fb2
thxru <59788043+thxru@users.noreply.github.com>
2020-08-25 23:57:09 +0530

    Create dbo.Accessories.sql

---
+++ dbo.Accessories.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Accessories] (
+    [AccessoryID]    NVARCHAR (50) NOT NULL,
+    [AccessoryType]  NVARCHAR (50) NOT NULL,
+    [AccessoryBrand] NVARCHAR (50) NOT NULL,
+    [Quantity]       NVARCHAR (50) NOT NULL,
+    [Price]          NVARCHAR (50) NOT NULL,
+    [Date]           NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([AccessoryID] ASC)
+);
+
