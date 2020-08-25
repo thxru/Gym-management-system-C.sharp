46a641bd4551baa3ee6a3c9c474d1cdc3c30781c
thxru <59788043+thxru@users.noreply.github.com>
2020-08-25 23:57:12 +0530

    Create dbo.Attendance.sql

---
+++ dbo.Attendance.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Attendance] (
+    [AttendanceID] NVARCHAR (50) NOT NULL,
+    [CustomerID]   NVARCHAR (50) NOT NULL,
+    [CustomerName] NVARCHAR (50) NOT NULL,
+    [Date]         NVARCHAR (50) NOT NULL,
+    [ArrivalTime]  NVARCHAR (50) NOT NULL,
+    [DepTime]      NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([AttendanceID] ASC)
+);
+
