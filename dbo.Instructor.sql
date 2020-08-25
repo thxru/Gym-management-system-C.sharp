a802ea5f95a3a6ffc556551946064bd904fe948e
thxru <59788043+thxru@users.noreply.github.com>
2020-08-25 23:57:20 +0530

    Create dbo.Instructor.sql

---
+++ dbo.Instructor.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Instructor] (
+    [InstructorID]   NVARCHAR (50) NOT NULL,
+    [InstructorName] NVARCHAR (50) NOT NULL,
+    [Address]        NVARCHAR (50) NOT NULL,
+    [NIC]            NVARCHAR (50) NOT NULL,
+    [Email]          NVARCHAR (50) NOT NULL,
+    [Phone]          NVARCHAR (50) NOT NULL,
+    [Gender]         NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([InstructorID] ASC)
+);
+
