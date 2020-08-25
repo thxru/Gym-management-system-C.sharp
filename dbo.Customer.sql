18d641889e4a4ee966855d9100d5be8f4806712e
thxru <59788043+thxru@users.noreply.github.com>
2020-08-25 23:57:16 +0530

    Create dbo.Customer.sql

---
+++ dbo.Customer.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [CustomerID]   NVARCHAR (50) NOT NULL,
+    [CustomerName] NVARCHAR (50) NOT NULL,
+    [Address]      NVARCHAR (50) NOT NULL,
+    [NIC]          NVARCHAR (50) NOT NULL,
+    [Email]        NVARCHAR (50) NOT NULL,
+    [Phone]        NVARCHAR (50) NOT NULL,
+    [Gender]       NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([CustomerID] ASC)
+);
+
