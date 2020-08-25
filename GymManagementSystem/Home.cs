0c1e58bed753394bbda1f08722a72824afb7dc60
thxru <59788043+thxru@users.noreply.github.com>
2020-08-25 23:58:54 +0530

    Create Home.cs

---
+++ GymManagementSystem/Home.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GymManagementSystem
+{
+    public partial class Home : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Pubz\Documents\GymDatabase.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlDataAdapter DA;
+        DataSet DS = null;
+        BindingSource bindingSource1 = new BindingSource();
+
+        public Home()
+        {
+            InitializeComponent();
+            LoadAllCustomer();
+           
+        }
+
+        private void LoadAllCustomer()
+        {
+            try
+            {
+                DS = new DataSet();
+                bindingSource1.DataSource = null;
+
+                con.Open();
+                string qry = "Select * from Customer";
+
+                DA = new SqlDataAdapter(qry, con);
+
+                DA.Fill(DS, "CustomerDetails");
+                bindingSource1.DataSource = DS.Tables["CustomerDetails"];
+
+                lblCustomerCount.Text = bindingSource1.Count.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occured : " + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+            try
+            {
+                DS = new DataSet();
+                bindingSource1.DataSource = null;
+
+                con.Open();
+                string qry = "Select * from Instructor";
+
+                DA = new SqlDataAdapter(qry, con);
+
+                DA.Fill(DS, "InstructorDetails");
+                bindingSource1.DataSource = DS.Tables["InstructorDetails"];
+
+                lblinstructorcount.Text = bindingSource1.Count.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occured : " + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+       
+        
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+            Login two = new Login();
+            this.Hide();
+            two.Show();
+        }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnCustomer_Click(object sender, EventArgs e)
+        {
+            Customer four = new Customer();
+            this.Hide();
+            four.Show();
+        }
+
+        private void btnInstructor_Click(object sender, EventArgs e)
+        {
+            Instructor five = new Instructor();
+            this.Hide();
+            five.Show();
+        }
+
+        private void btnAccessories_Click(object sender, EventArgs e)
+        {
+            Accessories six = new Accessories();
+            this.Hide();
+            six.Show();
+        }
+
+        private void btnAttendance_Click(object sender, EventArgs e)
+        {
+            Attendance seven = new Attendance();
+            this.Hide();
+            seven.Show();
+        }
+
+        private void btnPayment_Click(object sender, EventArgs e)
+        {
+            Payment eight = new Payment();
+            this.Hide();
+            eight.Show();
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lblCustomerCount_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
