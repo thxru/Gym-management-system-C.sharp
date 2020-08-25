16946f01ad7765aa99d6d8a9b2431fd8b3d79773
thxru <59788043+thxru@users.noreply.github.com>
2020-08-25 23:57:23 +0530

    Create dbo.Payment.sql

---
+++ dbo.Payment.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Payment] (
+    [PaymentID]       NVARCHAR (50) NOT NULL,
+    [CustomerID]      NVARCHAR (50) NOT NULL,
+    [CustomerName]    NVARCHAR (50) NOT NULL,
+    [Date]            NVARCHAR (50) NOT NULL,
+    [Amount]          NVARCHAR (50) NOT NULL,
+    [Paymentmethod]   NVARCHAR (50) NOT NULL,
+    [PaymentDuration] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([PaymentID] ASC)
+);
+
